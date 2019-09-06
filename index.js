d0057dfd19aaadda6d7481a71b8ff87daa7cd8d3
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-06 15:26:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file.
-function lowerCaseDrivers(drivers) {
+function lowerCaseDrivers(drivers, name) {
   return drivers.map(function (driverName){
-    return driverName.toLowerCase();
+    return driverName.name.toLowerCase();
   });
 }
   
