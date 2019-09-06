04efb2fa66a76b65b86d4f9c3c447a4f390773a6
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-06 15:30:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ function lowerCaseDrivers(drivers) {
 }
   
 function nameToAttributes(drivers) {
+   return drivers.map(function (driverName){
+     const driverFirst = driverName.split(' ')[0]
+   });
   
 }  
+
+function nameToAttributes(array) {
+  return array.map(function(driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+  return { firstName: driverFirst, lastName: driverLast };
+  });
+}
